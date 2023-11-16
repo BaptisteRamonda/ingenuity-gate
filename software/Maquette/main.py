d49c7147928e29834eae3141ffa7595fd7413678
BaptisteRamonda <71442501+BaptisteRamonda@users.noreply.github.com>
2023-11-16 15:54:40 +0100

    Revert "Ajout de commentaires et de docstring pour le code de la maquette"
    This reverts commit 06c8209744ec43e51ec64a5eb00f115855cb1b90.

--- software/Maquette/main.py
+++ software/Maquette/main.py
@@ -13,27 +13,16 @@ import ingescape as igs
 import serial
 import time
 
-# Global variables
+# global variables
 port = 'COM5'
 baudrate = 115200
-ser = None  # Serial port object
+ser = None
 
-lock = None  # Thread lock for synchronization
+lock = None
 
 
+# inputs
 def input_callback(iop_type, name, value_type, value, my_data):
-    """
-    Callback function for handling changes in the 'commandePortail' input.
-
-    Parameters:
-    - iop_type (int): Type of input or output (e.g., igs.INPUT_T, igs.OUTPUT_T).
-    - name (str): Name of the input or output.
-    - value_type (int): Data type of the value (e.g., igs.INTEGER_T, igs.BOOL_T).
-    - value: The new value of the input or output.
-    - my_data: Additional data (unused in this example).
-
-    This function is called when the 'commandePortail' input changes, and it sends the new value to the serial port.
-    """
     with lock:
         ser.reset_input_buffer()
         ser.reset_output_buffer()
@@ -41,7 +30,6 @@ def input_callback(iop_type, name, value_type, value, my_data):
 
 
 if __name__ == "__main__":
-    # Check if command line arguments are provided
     if len(sys.argv) < 4:
         print("usage: python3 main.py agent_name network_device port")
         devices = igs.net_devices_list()
@@ -50,34 +38,30 @@ if __name__ == "__main__":
             print(f" {device}")
         exit(0)
 
-    # Initialize the serial port
+    # start serial
     ser = serial.Serial(port, baudrate)
-    time.sleep(2)  # Allow time for the serial connection to stabilize
+    time.sleep(2)
 
-    # Initialize the thread lock
+    # init mutex
     lock = Lock()
 
-    # Configure the INGESCape agent
     igs.agent_set_name(sys.argv[1])
     igs.definition_set_version("1.0")
     igs.log_set_console(True)
     igs.log_set_file(True, None)
     igs.set_command_line(sys.executable + " " + " ".join(sys.argv))
 
-    # Define input and output variables
     igs.input_create("commandePortail", igs.INTEGER_T, None)
+
     igs.output_create("estOuvert", igs.BOOL_T, None)
     igs.output_create("estFerme", igs.BOOL_T, None)
     igs.output_create("estAppele", igs.BOOL_T, None)
     igs.output_create("distanceParcourue", igs.DOUBLE_T, None)
 
-    # Set up the callback function for the 'commandePortail' input
     igs.observe_input("commandePortail", input_callback, None)
 
-    # Start the INGESCape agent with the specified network device and port
     igs.start_with_device(sys.argv[2], int(sys.argv[3]))
 
-    # Main loop for reading data from the serial port and updating outputs
     while True:
         with lock:
             line = ser.readline()
@@ -85,11 +69,12 @@ if __name__ == "__main__":
                 decoded_line = line.decode()
                 datas = decoded_line.strip().split(',')
                 if len(datas) == 4:
-                    # Update the INGESCape outputs based on the received data
                     igs.output_set_double('distanceParcourue', float(datas[0]))
                     igs.output_set_bool('estOuvert', bool(int(datas[1])))
                     igs.output_set_bool('estFerme', bool(int(datas[2])))
                     igs.output_set_bool('estAppele', bool(int(datas[3])))
-
             except Exception:
                 pass
+    input()
+
+    igs.stop()
